6ae2ddfc45626ea89f8197d2e804f82be0d3c5c9
Benedikt Ehinger <behinger@uos.de>
2018-05-03 11:50:53 +0200

    minor change

--- code/sync_test_be.py
+++ code/sync_test_be.py
@@ -9,12 +9,12 @@ import os
 import matplotlib.pyplot as plt
 import functions.nbp_pupilhelper as nbp_pl
 from pyedfread import edf
-
+import functions.etcomp_parse as parse
 from functions import nbp_recalib
 
 #%%
 datapath = '/net/store/nbp/projects/etcomp/pilot'
-subject = 'bene'
+subject = 'inga_2'
 
 
 filename = os.path.join(datapath,subject,'raw')
@@ -42,45 +42,24 @@ def plot_trace(pupil,pos=0):
 # recalibration 
 pldata['gaze_positions'] = nbp_recalib.nbp_recalib(pldata)
 #%%
-def parse_message(msg):
-    
-    #print(msg)
-    try:
-        time = msg['time']
-        string = msg['trialid '] # space on purpose
-        
-    except:
-        try:
-            time = msg['timestamp']
-            string = msg['label']
-        except:
-                return(np.nan)
-    
-    split = string.split(' ')    
-    parsedmsg = pd.DataFrame()
-    if split[0] == 'GRID' and split[1] == 'element':
-        parsedmsg = dict(
-              time = time,  
-              id=int(split[2]),
-             posx = float(split[4]),
-             posy = float(split[6]),
-             total = int(split[8]),
-             block = int(split[10])
-                )
-    return(pd.Series(parsedmsg))
     
     
     
 def match_data(et,msgs,td=2):
-    # Input: et(DataFrame) input data of the eyetracker
-    #        msgs(DataFrame) already parsed input messages e.g. 'GRID element 5 pos-x 123 ...' defining experimental events
-    # Output: Data.frame for each notification, find all samples that are in the range of +- td (default 2 s)
-    pd_matched_data = pd.DataFrame()
+    # Input: et(DataFrame) input data of the eyetracker (has column smpl_time)
+    #        msgs(DataFrame) already parsed input messages e.g. 'GRID element 5 pos-x 123 ...' defining experimental events (has column msg_time)
+    # Output: Data.frame for each notification, find all samples that are in the range of +-td (default timediff 2 s)
+    matched_data = pd.DataFrame()
+    
     for idx,msg in msgs.iterrows():
         print(idx)
-        ix = abs(et['time'] - msg['time'])<td # ix is a boolean (0 / 1, false / true)
+        ix = abs(et['smpl_time'] - msg['msg_time'])<td # ix is a boolean (0 / 1, false / true) (this way we find all samples +-td)
+        if np.sum(ix) == 0:
+            print('warning, no sample found for msg %i'%(idx))
+            print(msg)
+            continue
         tmp= et.loc[ix]
-        tmp = tmp.assign(td=tmp.time-msg['time'])
+        tmp = tmp.assign(td=tmp.smpl_time-msg['msg_time'])
     
         
         msg_tmp = pd.concat([msg.to_frame()]*tmp.shape[0],axis=1).T
@@ -88,37 +67,45 @@ def match_data(et,msgs,td=2):
         
         
         tmp = pd.concat([tmp,msg_tmp],axis=1)
-        pd_matched_data = pd_matched_data.append(tmp)
-    return(pd_matched_data)
+        matched_data = matched_data.append(tmp)
+   
+    return(matched_data)
+    
+  
     
 def findFile(path,ftype):
     out = [edf for edf in os.listdir(path) if edf.endswith(ftype)]
     return(out)
 
-def plotTraces(et,query = 'posx == 960',figure = True):
+def plotTraces(et,field = 'gx',query = 'posx == 960',figure = True):
     from bokeh.plotting import figure,show
-    from bokeh.models import Span, CrosshairTool, HoverTool, ResetTool, PanTool, WheelZoomTool
+    from bokeh.models import Span, CrosshairTool, HoverTool, ResetTool, PanTool, BoxZoomTool,WheelZoomTool
+    from bokeh.transform import factor_cmap
+    from bokeh.palettes import Spectral6
+
+
     TOOLS = [CrosshairTool(dimensions='both'),
-             HoverTool(),PanTool(),
+             HoverTool(),PanTool(),BoxZoomTool(),
              ResetTool()]
     if type(et) != list:
         et = [et]
     p = figure()
     for ix,e in enumerate(et):
-        et[ix] = e.query(query) 
-        et[ix] = et[ix][['gx','td','group']]
-        et[ix]['group'] = ix
+        #et[ix] = e.query(query) 
+        et[ix]['group'] = str(ix)
+        #et[ix] = et[ix][[field,'td','group']]
+
         
         
     tmp = pd.concat(et,ignore_index=True,)
+    tmp = tmp.query(query)
      #   if figure:
             #plt.figure()
-
-    p = figure(width=800,tools=TOOLS)
-    p.circle(x=tmp.td,y=tmp.gx,color=tmp.group)
+    
+    p = figure(width=1400,tools=TOOLS)
+    p.circle(x='td',y=field,,source = tmp,legend='group',color=factor_cmap('group',factors=['0','1'], palette=Spectral6))
     show(p)        
-    
-    
+    #%%
 
 #%%    
 
@@ -126,7 +113,7 @@ gridnotes = [note for note in pldata['notifications'] if 'label' in note.keys() 
 
 pd_plmsgs = pd.DataFrame();
 for note in gridnotes:
-    msg = parse_message(note)
+    msg = parse.parse_message(note)
     if not msg.empty:
         pd_plmsgs = pd_plmsgs.append(msg, ignore_index=True)
 
@@ -141,23 +128,36 @@ pd_pl_matched_data = match_data(pd_pldata,pd_plmsgs)
     
 
 #%%
-elsamples, elevents,elnotes = edf.pread(os.path.join(filename,findFile(filename,'.EDF')[0]), trial_marker=b'')
-# change to seconds to be the same as pupil
-elsamples['time'] = elsamples['time']/1000
-elnotes['time'] = elnotes['trialid_time']/1000
-elnotes = elnotes.drop('trialid_time',axis=1)
-elevents['time'] = elevents['time']/1000
+elsamples, elevents, elnotes = edf.pread(os.path.join(filename,findFile(filename,'.EDF')[0]), trial_marker=b'')
 
+# change to seconds to be the same as pupil
+elsamples['smpl_time'] = elsamples['time']/1000
+
+elnotes['msg_time'] = elnotes['trialid_time']/1000
+elnotes = elnotes.drop('trialid_time',axis=1)
+
+elevents['msg_time'] = elevents['time']/1000
+
+# determine recorded eye
 # if more than 90% of samples are on the the left, assume everything was recorded left
 elsamples['gx']  = elsamples.gx_left if np.mean(elsamples.gx_left != -32768)>0.9 else elsamples.gx_right
+elsamples['gy']  = elsamples.gy_left if np.mean(elsamples.gy_left != -32768)>0.9 else elsamples.gy_right
 
-#
-pd_elmsgs = elnotes.apply(parse_message,axis=1)
-pd_elmsgs = pd_elmsgs.dropna()
-pd_el_matched_data = match_data(elsamples,pd_elmsgs)
+
+# TODO grab eye with pa 
+# convert to diameter
+
+# parse EL msg and match data to get pandas df
+elmsgs = elnotes.apply(parse.parse_message,axis=1)
+elmsgs = elmsgs.drop(elmsgs.index[elmsgs.isnull().all(1)])
+
+
+# match the et data to the msgs (match smpl_time to msg_time)
+el_matched_data = match_data(elsamples,elmsgs)
+
 #%%
-pd_pl_matched_data.gx = pd_pl_matched_data.gx*1920
-pd_pl_matched_data.gy = pd_pl_matched_data.gy*1280
+#pd_pl_matched_data.gx = pd_pl_matched_data.gx*1920
+#pd_pl_matched_data.gy = pd_pl_matched_data.gy*1280
 #%%
 
 plt.figure()
@@ -171,21 +171,16 @@ plotTraces([pd_pl_matched_data,pd_el_matched_data],query = 'id == 15 & block == 
 #%%
 
 pl2 = pd_pl_matched_data.copy()
-pl2['td'] = pl2['td']-0.15 #for Katha pilot 0.02 (absolute diff in clock was 50ms in ~450seconds)
+pl2['td'] = pl2['td'] #for Katha pilot 0.02 (absolute diff in clock was 50ms in ~450seconds)
 #pl2['gx'] = pl2['gx']
 #%%
-el2 = pd_el_matched_data[abs(pd_el_matched_data['gx'])<2500]
-el2['gx'] = (el2['gx']/1920 - 0.5) * 0.5 + 0.42
+el2 = el_matched_data[abs(el_matched_data['gx'])<2500]
+el2['gx'] = (el2['gx']/1920 - 0.5) * 0.55 + 0.52
+el2.diameter = el2.pa_right
 #%%
-plt.figure()
+plotTraces([el2,pl2],query='gx<10000 & grid_size==49 & block ==1& element==5')
 
-plotTraces([pl2,el2],query = 'block == 1& total==49 & id ==1',figure=False)
-plt.figure()
-plotTraces([pl2,el2],query = 'block == 1& total==13 & id ==13',figure=False)
-plt.figure()
-plotTraces([pl2,el2],query = 'block == 2& total==49 & id ==49',figure=False)
-
-
+plotTraces([el2,pl2],field='diameter',query='diameter>0  & lum==1')
 
 #%% ---- Have a look at the surface CSV files
 # In the end they look super bad, the eyes are not fused properly.
