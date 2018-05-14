a9284ebb18e824bfcf9d2def756c5c415ad5c492
Benedikt Ehinger <behinger@uos.de>
2018-05-14 17:40:08 +0200

    change dpupildilation once more, 10s dark + 3s light

--- experiment/expPupilDilation.m
+++ experiment/expPupilDilation.m
@@ -4,16 +4,51 @@ showInstruction('DILATION',screen,requester,eyetracking, block);
 dilation.win = screen.win;
 sendETNotifications(eyetracking,requester,sprintf('DILATION start, block %d',block));
 
+newVersion = 1
+if newVersion
+
+  for color_id = 1:length(randomization)
+      dilation.background_color = randomization(color_id);
+
+      Screen('FillRect', screen.win, [randomization(color_id) randomization(color_id) randomization(color_id)]);
+      drawTarget(screen.screen_width/2, screen.screen_height/2,dilation,20,'fixbulleye');
+
+      if randomization(color_id) == 0 %if black, wait 10s, else wait 3 second
+        waitBaseTime = 10;
+      else
+        waitBaseTime = 3;
+      end
+
+      if color_id == 1
+        LastFlip = flip_screen(screen, 0);
+      else
+        LastFlip = flip_screen(screen, LastFlip + waitBaseTime + rand(1) * 0.5 - 0.25);
+      end
+      sendETNotifications(eyetracking,requester,sprintf('DILATION lum %d block %d',randomization(color_id),block))
+
+
+
+  end
+
+else
+  %run old version
 for color_id = 1:length(randomization)
     dilation.background_color = randomization(color_id);
 
     Screen('FillRect', screen.win, [randomization(color_id) randomization(color_id) randomization(color_id)]);
     drawTarget(screen.screen_width/2, screen.screen_height/2,dilation,20,'fixbulleye');
-    LastFlip = flip_screen(screen, 0);
-    flip_screen(screen, LastFlip + 5);
 
+    if color_id == 1
+      LastFlip = flip_screen(screen, 0);
+    else
+      LastFlip = flip_screen(screen, LastFlip + 7.5 + rand(1) * 0.5 - 0.25);
+    end
     sendETNotifications(eyetracking,requester,sprintf('DILATION lum %d block %d',randomization(color_id),block))
-    
+
+
+
 end
+
+end % newversion if
 
 sendETNotifications(eyetracking,requester,sprintf('DILATION stop, block %d', block));
--- experiment/randomize_stimuli.m
+++ experiment/randomize_stimuli.m
@@ -7,8 +7,10 @@ numberimagesperblock = 3;
 randomization = load('randomization_larger_grid.mat');
 rand_grid_large=randomization.ix;
 
-monitorluminanceLookup = [0,64,192,255];
-% 1.5s per value => 7.5s, in 30s => 5 repetitions
+monitorluminanceLookup = [64,128, 192,255];
+dilation_blank_color = 0;% 0 is black
+
+% currently we can make only 4 blocks, ~52s.
 monitorluminance = repmat(1:length(monitorluminanceLookup),1,1);
 
 %smooth pursuit
@@ -36,47 +38,47 @@ end
 
 % generate randomization
 for subject = subjectlist
-    
+
     largeBlockPerm = randperm(max_block);
     smoothPerm = randperm(length(smooth_speed_sub));
     freeviewing = randperm(max_block*numberimagesperblock);
-    
+
     for block = 1:max_block
-        
+
         % accuracy tests / grids
         rand.smallBefore       = [rand.smallBefore {randperm(13)}];
         rand.smallAfter        = [rand.smallAfter  {randperm(13)}];
         rand.large             = [rand.large       {rand_grid_large(largeBlockPerm(block),:)}];
-        
-        
+
+
         % every luminance interdisperced by gray
         ix = randperm(length(monitorluminance));
-        lum = ones(length(monitorluminance)*2,1)*128;
+        lum = ones(length(monitorluminance)*2,1)*dilation_blank_color;
         lum(2:2:2*length(monitorluminance)) = monitorluminanceLookup(monitorluminance(ix));
         rand.pupildilation = [rand.pupildilation {lum}];
-        
-        
+
+
         % smooth pursuit
         smooth_ix(1) = (block-1) * smooth_trials_per_block + 1;
         smooth_ix(2) = (block) * smooth_trials_per_block;
         rand.smoothpursuit_speed = [rand.smoothpursuit_speed {smooth_speed_sub(smoothPerm(smooth_ix(1):smooth_ix(2)))}];
         rand.smoothpursuit_angle = [rand.smoothpursuit_angle  {smooth_angle_sub(smoothPerm(smooth_ix(1):smooth_ix(2)))}];
-        
+
         free_ix(1) = (block-1)*3+1;
         free_ix(2) = (block)*3;
         rand.freeviewing = [rand.freeviewing {freeviewing(free_ix(1):free_ix(2))}];
-        
+
         % shake and Tilt
         movements = {'SHAKE','TILT'};
         rand.firstmovement =[rand.firstmovement  movements(1+mod(subject+block,2))];
-               
+
         % shake
         while true
             % make sure no two following numbers are equal
             ix = randperm(length(shake_points));
             if all(diff(shake_points(ix))~=0)
                 break
-                
+
             end
         end
         rand.shake = [rand.shake {shake_points(ix)}];
@@ -86,18 +88,18 @@ for subject = subjectlist
             ix = randperm(length(tilt_angles));
             if all(diff(tilt_angles(ix))~=0)
                 break
-                
+
             end
         end
         rand.tilt = [rand.tilt {tilt_angles(ix)}];
-        
-        
+
+
         rand.block =    [rand.block block];
         rand.subject = [rand.subject subject];
 
-        
+
     end
-    
+
 end
 
 
