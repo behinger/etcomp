c098e8d9d40e8a1e7e2169b53b92aa9fc7d9e3b7
Benedikt Ehinger <behinger@uos.de>
2018-05-22 12:35:10 +0200

    added fake uvc pupillabs

---
+++ code/functions/fake_pl_loading/old_file_methods.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def save_object(obj,path):
+        print('fake saving to folder:'+path)
+        
+def load_object(obj,path):
+        print('fake loading to folder:'+path)
+        return([])
