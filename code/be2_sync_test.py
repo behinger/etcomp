4f6c4ad86869c3e53b5ac39c1901cb796262767f
Benedikt Ehinger <behinger@uos.de>
2018-06-01 12:33:02 +0200

    adde new plots

--- code/be2_sync_test.py
+++ code/be2_sync_test.py
@@ -39,7 +39,15 @@ elsamples, elmsgs, elevents = preprocess.preprocess_et('el',subject,load=True)
 
 elsamples = detect_bad_samples.remove_bad_samples(elsamples)
 plsamples = detect_bad_samples.remove_bad_samples(plsamples)
+#%%
+# THIS IS DANGEROUS BECAUSE I CHANGE THE TIMESTAMPS ONLY IN THE SAMPLES! NOT THE EVENTS
+elsamples.smpl_time = elsamples.smpl_time - min(elsamples.smpl_time)
+plsamples.smpl_time = plsamples.smpl_time - min(plsamples.smpl_time)
+etsamples = pd.concat([elsamples.assign(eyetracker='eyelink'),plsamples.assign(eyetracker='pupillabs')],ignore_index=True, sort=False)
 
+
+#%%
+ggplot(etsamples,aes(x='smpl_time',y='gx',color='type',shape='eyetracker')) + geom_point()
 #%%
 condquery = 'condition == "DILATION" & exp_event=="lum"'
 td = [-1, 15]
@@ -79,3 +87,31 @@ ggplot(etepochs_smooth,aes(x='td',y='gx',color='angle'))+\
             geom_vline(xintercept=[0,0.2]) +\
             facet_wrap('~eyetracker') # split up by 
             
+
+#%%
+condquery = 'condition == "FREEVIEW" & exp_event=="trial"'
+td = [-0.2, 6]
+elepochs_fv = make_epochs(elsamples,elmsgs.query(condquery), td=td)
+plepochs_fv= make_epochs(plsamples,plmsgs.query(condquery), td=td)
+
+
+# Concatenate for easy ggplotting
+etepochs_fv = pd.concat([elepochs_fv.assign(eyetracker='eyelink'),plepochs_fv.assign(eyetracker='pupillabs')],ignore_index=True)
+
+# every 10th sample is enough
+ggplot(etepochs_fv,aes(x='gx',y='gy'))+\
+            geom_bin2d() +\
+            geom_point(color='white') +\
+            facet_wrap('~eyetracker') # split up by 
+            
+#%% An example to plot fixation durations in comparison
+etevents= pd.concat([elevents.assign(eyetracker='eyelink'),plevents.assign(eyetracker='pupillabs')],ignore_index=True)
+            
+#Fixduration
+ggplot(etevents.query('type=="fixation"'),aes(x='duration',color='eyetracker'))+geom_density()+xlim([0,1])
+
+#Saccadeparameters
+ggplot(etevents.query('type=="saccade"'),aes(x='amplitude',color='eyetracker'))+geom_density() + xlab('amplitude [px]')
+
+# main sequence
+ggplot(etevents.query('type=="saccade"'),aes(x='np.log10(peak_velocity)',y='np.log10(amplitude)',color='eyetracker'))+stat_smooth(method='loess')
