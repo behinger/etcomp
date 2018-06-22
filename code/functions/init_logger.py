ad637c5af1acf218a8de5d38ea815db4098dce12
Benedikt Ehinger <behinger@uos.de>
2018-06-22 15:28:12 +0200

    changed logger to be subjectwise (if wanted) and added grid scripts to run preprocessing on grid

--- code/functions/init_logger.py
+++ code/functions/init_logger.py
@@ -22,11 +22,11 @@ if len(logger.handlers) != 2:
     logfile = os.path.join('/net/store/nbp/projects/etcomp/log_files', str('temp_' + time.strftime("%Y_%m_%d-%H-%M-%S") + '.log'))
     
     # final logfile name
-    #logfile = os.path.join('/net/store/nbp/projects/etcomp/log_files', str('log_preprocess_' + time.strftime("%Y_%m_%d-%H-%M-%S") + '.log'))
+    logfile = os.path.join('/net/store/nbp/projects/etcomp/log_files', str('log_preprocess_' + time.strftime("%Y_%m_%d-%H-%M-%S") + '.log'))
     
     # delete file if it already exists
-    #if os.path.isfile(logfile):
-    #    os.remove(logfile)
+    if os.path.isfile(logfile):
+        os.remove(logfile)
     
     # define handlers    
     logging_file = logging.FileHandler(filename=logfile)
@@ -52,3 +52,28 @@ else:
 # To close
 #[h.close() for h in logger.handlers]
 #[logger.removeHandler(h) for h in logger.handlers]
+
+
+def update_logger_filepath(newpath):
+    # delete old filehandler
+    logger = logging.getLogger()
+    for hdlr in logger.handlers[:]:  # remove all old handlers
+        if isinstance(hdlr,logger.FileHander):
+            logger.removeHandler(hdlr)
+    
+    # delete file if it already exists
+    if os.path.isfile(newpath):
+        os.remove(newpath)
+    
+    # define handlers    
+    logging_file = logging.FileHandler(filename=newpath)
+    
+    # set handler level
+    logging_cons.setLevel(logging.DEBUG)
+    
+    # create a logging format
+    formatter = logging.Formatter("%(asctime)s - %(name)-65s - %(levelname)-8s - %(message)s", "%Y-%m-%d %H:%M:%S")
+    logging_file.setFormatter(formatter)
+    
+    # add the handlers to the logger
+    logger.addHandler(logging_file)
---
+++ code/grid_preprocess.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 13:23:55 2018
+
+@author: behinger
+"""
+
+import functions.add_path
+
+import sys
+sys.path
+import functions.init_logger as init_logger
+import logging
+import functions.et_helper as  helper
+
+import os
+import time
+
+import functions.et_preprocess as preprocess
+from functions.detect_events import make_blinks,make_saccades,make_fixations
+
+
+# loop over the foldernames (subjectnames)
+# restricted to subjects that we do not exclude from analysis
+# also loop over the et
+foldernames       = helper.get_subjectnames('/net/store/nbp/projects/etcomp/')
+logfilepath = '/net/store/nbp/projects/etcomp/log_files/'
+rejected_subjects = ['pilot', 'log_files', 'surface', '007', 'VP8']
+# ['pilot', '007', 'log_files', 'surface', 'VP1', 'VP2', 'VP3', 'VP4', 'VP7', 'VP8', 'VP11', 'VP12', 'VP14', 'VP15']
+# ['pilot', '007', 'log_files', 'surface', 'VP1', 'VP7', 'VP8', 'VP11', 'VP12', 'VP14', 'VP15']
+#rejected_subjects = ['pilot', 'log_files', 'surface', '007', 'VP8', 'VP1', 'VP2', 'VP7', 'VP8', 'VP11', 'VP12', 'VP14', 'VP15']
+subjectnames      = [subject for subject in foldernames if subject not in rejected_subjects]
+    
+    
+if 1 == 0:
+    import subprocess 
+    subprocess.check_output(["qsub",'-cwd','-N','etcomp','-t','%i:%i'%(1,len(subjectnames)),'-l','mem=15G,h=!ramsauer.ikw.uni-osnabrueck.de','-e',logfilepath,'-o',logfilepath,'grid_preprocess.sge'])
+    
+
+
+print(os.environ['SGE_TASK_ID'])
+subid = int(os.environ['SGE_TASK_ID'])-1
+print(subid)
+subject = subjectnames[subid] #jobID ranges from 1:N not 0:N-1
+# change the loggerpath
+
+init_logger.update_logger_filepath(newpath = os.path.join(logfilepath, 'log_preprocess_%s_subject%s.log'%(time.strftime("%Y_%m_%d-%H-%M-%S"),subject)))
+logger = logging.getLogger(__name__)
+ets               = ['pl', 'el']    
+# preprocess for all subjects
+
+for et in ets:
+        logger.critical(' ')
+        logger.critical('Eyetracker: %s    Subject: %s ', et, subject)
+        etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,make_saccades,make_fixations))
