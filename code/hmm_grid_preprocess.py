50e4723d4b2af55414d4be6d2c36cc2bae71ebad
Benedikt Ehinger <behinger@uos.de>
2018-08-10 15:36:06 +0200

    multiple configurations possible to run

--- code/hmm_grid_preprocess.py
+++ code/hmm_grid_preprocess.py
@@ -19,7 +19,7 @@ import time
 
 import functions.et_preprocess as preprocess
 from functions.detect_events import make_blinks,make_saccades,make_fixations
-from functions.detect_events_hmm import detect_events_hmm
+from functions.detect_events_hmm import detect_events_hmm,detect_events_hmm_nosmooth    
 
 os.chdir(wd)
 # loop over the foldernames (subjectnames)
@@ -32,23 +32,33 @@ logfilepath = '/net/store/nbp/projects/etcomp/log_files/'
 # also loop over the et
 foldernames       = helper.get_subjectnames('/net/store/nbp/projects/etcomp/')
 #TODO find out whats wrong with vp3 and vp12 and fix and then use vp3 again!!
-rejected_subjects = ['pilot', 'log_files', 'surface', '007', 'VP8', 'VP21','VP7']
+rejected_subjects = ['pilot', 'log_files', 'surface', 'results','007', 'VP8', 'VP21','VP7']
 subjectnames      = [subject for subject in foldernames if subject not in rejected_subjects]
 ets               = ['el', 'pl']
 #subjectnames = [subjectnames[i] for i in [9,14]]
 #subjectnames = ['VP20']
 
 
+
+
 print(sys.argv)
-if len(sys.argv)>1 and sys.argv[1] == 'startgrid':
-    
+if len(sys.argv)>1:
+    job_name = sys.argv[1]
+    assert(job_name in ['etcomp','etcomp_hmm','etcomp_hmm_nosmooth'])
     import subprocess 
-    subprocess.check_output(["qsub",'-cwd','-N','etcomp_hmm','-t','%i:%i'%(1,len(subjectnames)),'-l','mem=20G,h=!ramsauer.ikw.uni-osnabrueck.de','-e',logfilepath,'-o',logfilepath,'-q','nbp.q','hmm_grid_preprocess.sge'])
+    if len(sys.argv)>2:
+        subprocess.check_output(["qsub",'-cwd','-N',job_name,'-t',sys.argv[2],'-l','mem=20G,h=!ramsauer.ikw.uni-osnabrueck.de','-e',logfilepath,'-o',logfilepath,'-q','nbp.q','hmm_grid_preprocess.sge'])
+    else:
+        subprocess.check_output(["qsub",'-cwd','-N',job_name,'-t','%i:%i'%(1,len(subjectnames)),'-l','mem=20G,h=!ramsauer.ikw.uni-osnabrueck.de','-e',logfilepath,'-o',logfilepath,'-q','nbp.q','hmm_grid_preprocess.sge'])
+        
+    
     sys.exit() 
 
-
+job_name = os.environ['JOB_NAME']
 print(os.environ['SGE_TASK_ID'])
 subid = int(os.environ['SGE_TASK_ID'])-1
+
+print(job_name)
 print(subid)
 subject = subjectnames[subid] #jobID ranges from 1:N not 0:N-1
 # change the loggerpath
@@ -62,5 +72,11 @@ ets               = ['pl', 'el']
 for et in ets:
         logger.critical(' ')
         logger.critical('Eyetracker: %s    Subject: %s ', et, subject)
-        #etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,make_saccades,make_fixations))
-        etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,detect_events_hmm),outputprefix='hmm_')
+        if job_name == 'etcomp':
+            etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,make_saccades,make_fixations ),outputprefix='')
+        elif job_name == 'etcomp_hmm':
+            etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,detect_events_hmm),outputprefix='hmm_')
+        elif job_name == 'etcomp_hmm_nosmooth':
+            etsamples, etmsgs, etevents = preprocess.preprocess_et(et,subject,load=False,save=True,eventfunctions=(make_blinks,detect_events_hmm_nosmooth),outputprefix='hmmnosmooth_')
+        else:
+            raise('Unknown jobname')
