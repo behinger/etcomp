a393b851fe75f3b381faa564dcf1096940f01afa
sitimm <sitimm@isonoe.neurobiopsychologie.uni-osnabrueck.de>
2018-11-26 18:49:53 +0100

    nothing changed after creating new branch

--- run_etcomp_jupyter.sh
+++
@@ -1,5 +0,0 @@
-export LD_LIBRARY_PATH=/net/store/nbp/projects/IntoTheWild/ET_Analysis/etcomp/local/build/build_ffmpeg/lib/:/net/store/nbp/projects/IntoTheWild/ET_Analysis/etcomp/local/build/build_glfw/lib/:/local/build/build_ceres/lib:/net/store/nbp/projects/IntoTheWild/ET_Analysis/etcomp/local/build/build_opencv/lib:$LD_LIBRARY_PATH
-
-source local/etcompvenv/bin/activate &&\
-jupyter lab 
-
