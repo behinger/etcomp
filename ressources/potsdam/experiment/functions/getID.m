36de9a4ce7536e2e103f295400a5ed711e94671d
kgross <kgross@uos.de>
2018-12-09 23:49:16 +0100

    removed old resources

--- ressources/potsdam/experiment/functions/getID.m
+++
@@ -1,9 +0,0 @@
-function [const]=getID
-% get subject ID
-
-const.vpnr = str2num(input('Wie lautet die Versuchspersonen-Nummer? ','s'));
-const.code = sprintf('SpStMob_%.2d',const.vpnr);
-if isempty(const.vpnr)
-    const.vpnr = 00;
-    const.code = sprintf('SpStMob_%.2d',const.vpnr);
-end
