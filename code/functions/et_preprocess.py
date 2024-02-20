3c98dabbeb2b19547c34f3d55d679cab35c195f6
ac140358 <anna.pryslopska@ling.uni-stuttgart.de>
2024-02-20 12:46:38 +0100

    Removed PL reference, added TPX

--- code/functions/et_preprocess.py
+++ code/functions/et_preprocess.py
@@ -43,22 +43,19 @@ def preprocess_et(et,subject,participant_info,datapath='/net/store/nbp/projects/
 
         
     # import according to the type of eyetracker
-    logger.debug("Importing et data")
-    if et == 'pl':
-        logger.debug('Caution: etevents might be empty')
-        etsamples,etmsgs,etevents = import_pl(subject=subject,datapath=datapath, **kwargs)
-    elif et == 'el':
+    logger.debug("Importing eyetracking data")
+    if et == 'el':
         from functions.et_import import import_el
-        etsamples,etmsgs,etevents = import_el(subject=subject,participant_info=participant_info,datapath=datapath, **kwargs)
+        etsamples, etmsgs, etevents = import_el(subject=subject, participant_info=participant_info, datapath=datapath, **kwargs)
     elif et =="tpx":
         from functions.et_import import import_tpx
-        etsamples,etmsgs,etevents = import_tpx(subject=subject,participant_info=participant_info,datapath=datapath, **kwargs)
+        etsamples, etmsgs, etevents = import_tpx(subject=subject, participant_info=participant_info, datapath=datapath, **kwargs)
     else:
-        raise ValueError("Eyetracker must be el or pl!") 
+        raise ValueError("Eyetracker must be 'el' or 'tpx'!") 
         
         
     # Mark bad samples
-    logger.debug('Marking bad et samples')
+    logger.debug('Marking bad eyetracking samples')
     etsamples = detect_bad_samples(etsamples)
     
     # Detect events
